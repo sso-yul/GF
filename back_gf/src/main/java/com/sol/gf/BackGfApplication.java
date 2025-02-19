3e125500f049a949e64a72f6b30c5dec82ebeec6
sso-yul <ssoyul987@gmail.com>
2025-02-19 10:29:57 +0900

    백/프론트 폴더 생성

---
+++ back_gf/src/main/java/com/sol/gf/BackGfApplication.java
@@ -0,0 +1,13 @@
+package com.sol.gf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGfApplication.class, args);
+	}
+
+}
